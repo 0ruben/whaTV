9388186fc4578198731b44dca279ed752de46b90
totojab <eliottjabes@gmail.com>
2015-11-17 19:27:19 +0200

    visibility improvement

--- api/controllers/KeywordController.js
+++ api/controllers/KeywordController.js
@@ -6,22 +6,20 @@
  */
 
 module.exports = {
-	
-	getKeywords:function(req, res){
-		Keyword.query("select str, count(*) as compteur from keyword where str like '%"+ToolsService.clean(req.param('keyword'))+"%' and user<>"+req.param('user')+" group by str order by compteur desc limit "+req.param('limit')).exec(function(err, keywords){
-			if(err){
-				console.log(err);
-				return res.status(400).end();
-			}
-			else
-				return res.status(200).json(keywords);
-		});
 
-	},
+    getKeywords: function(req, res) {
+        Keyword.query("select str, count(*) as compteur from keyword where str like '%" + ToolsService.clean(req.param('keyword')) + "%' and user<>" + req.param('user') + " group by str order by compteur desc limit " + req.param('limit')).exec(function(err, keywords) {
+            if (err) {
+                console.log(err);
+                return res.status(400).end();
+            } else
+                return res.status(200).json(keywords);
+        });
+
+    },
 
 
 
 
 
 };
-
--- api/services/ScrapperService.js
+++ api/services/ScrapperService.js
@@ -1,94 +1,102 @@
-			var cheerio = require('cheerio');
-			var request = require('request');
-			var iconv = require('iconv-lite');
+var cheerio = require('cheerio');
+var request = require('request');
+var iconv = require('iconv-lite');
 
 
 
-			module.exports={
+module.exports = {
 
-				getProgrammes:function(date,heure,callbackGeneral){
-					var dayData=[];
-					var urls =[];
-					request({encoding: null,url:'http://programme-tv.com/'+date+'/tnt/'+heure+'/mon-programme-television.html'}, function (error, response, html) {
-						console.log(error);
-						if (!error && response.statusCode == 200) {
-							html = new Buffer(html);
+    getProgrammes: function(date, heure, callbackGeneral) {
+        var dayData = [];
+        var urls = [];
+        request({
+            encoding: null,
+            url: 'http://programme-tv.com/' + date + '/tnt/' + heure + '/mon-programme-television.html'
+        }, function(error, response, html) {
+            console.log(error);
+            if (!error && response.statusCode == 200) {
+                html = new Buffer(html);
 
-							html = iconv.decode(html,'iso-8859-1');
-							console.log(html);
-							var $ = cheerio.load(html); // $ est la page où il y a tous les programmes de l'horaire donné
-
-
-							urls= _.pluck(_.pluck($('h4').find('a'), 'attribs')  ,'href');
-							console.log(urls);
-
-							for (var i = urls.length - 1; i >= 0; i--) {
-								urls[i] = 'http://www.programme-tv.com/mon-programme-television.html' + urls[i];
-							}
-
-								//	console.log(urls);
+                html = iconv.decode(html, 'iso-8859-1');
+                console.log(html);
+                var $ = cheerio.load(html); // $ est la page où il y a tous les programmes de l'horaire donné
 
 
-							}
-							Keyword.find(function(err,keyWords){
-								keyWords = _.uniq(_.pluck(keyWords,'cleanname'));
+                urls = _.pluck(_.pluck($('h4').find('a'), 'attribs'), 'href');
+                console.log(urls);
+
+                for (var i = urls.length - 1; i >= 0; i--) {
+                    urls[i] = 'http://www.programme-tv.com/mon-programme-television.html' + urls[i];
+                }
+
+                //	console.log(urls);
 
 
-								async.eachLimit(urls,50,function(elem,callback){
+            }
+            Keyword.find(function(err, keyWords) {
+                keyWords = _.uniq(_.pluck(keyWords, 'cleanname'));
 
-									request({encoding:null, url:elem}, function (error, response, html) {
-										if (!error && response.statusCode == 200) {
-											
-											html = new Buffer(html);
-											html = iconv.decode(html,'iso-8859-1');
-											var $ = cheerio.load(html);
 
-											var data = {};
+                async.eachLimit(urls, 50, function(elem, callback) {
 
-											tailleCourante = dayData.length-1;
+                    request({
+                        encoding: null,
+                        url: elem
+                    }, function(error, response, html) {
+                        if (!error && response.statusCode == 200) {
 
-											data.chaine = $('img[class="img-chaine"]').attr("alt");
+                            html = new Buffer(html);
+                            html = iconv.decode(html, 'iso-8859-1');
+                            var $ = cheerio.load(html);
 
-											data.heure = $('p[class="label-heure"]').text();
-											var texteDuree =  $('p[class="programme-suite"]').text();
-											texteDuree = texteDuree.substring(texteDuree.indexOf(':'),texteDuree.indexOf('min'));
-											// data.duree = texteDuree;
-											data.titre = $('span[itemprop="name"]').text();
-											data.soustitre = $('span[itemprop="alternativeHeadline"]').text();
+                            var data = {};
 
-											data.description = $('p[itemprop="description"]').text();
-											data.casting = $('div[class="programme-casting onglet"]').text();
-											dayData.push(data);
-											ScrapperService.createProgramme(data,keyWords,function(){
-												callback();
-											});
-										}
-									});
-		},function(){
-			callbackGeneral(dayData);
-		});
+                            tailleCourante = dayData.length - 1;
 
-		});	
-		});
-		},
+                            data.chaine = $('img[class="img-chaine"]').attr("alt");
 
-		createProgramme : function(data,keyWords,callback){
-			Programme.create(data,function(err,programme){
-				var clean = ToolsService.clean(programme.description)+" "+ToolsService.clean(programme.titre);
-				var keywords = _.filter(keyWords, function(keyword){
-					return clean.indexOf(keyword)>-1;
-				});
-				console.log(keywords);
-				if(keywords.length>0){
-					_.each(keywords,function(keyword,index){
-						KeywordProgramme.create({keyword: keyword,programme: programme.id},function(err,elem){
-							if(index == keywords.length-1)
-								callback();
-						});
-					});
-				}
-				else 
-					callback();
-			});
-		}
-	}
+                            data.heure = $('p[class="label-heure"]').text();
+                            var texteDuree = $('p[class="programme-suite"]').text();
+                            texteDuree = texteDuree.substring(texteDuree.indexOf(':'), texteDuree.indexOf('min'));
+                            // data.duree = texteDuree;
+                            data.titre = $('span[itemprop="name"]').text();
+                            data.soustitre = $('span[itemprop="alternativeHeadline"]').text();
+
+                            data.description = $('p[itemprop="description"]').text();
+                            data.casting = $('div[class="programme-casting onglet"]').text();
+                            dayData.push(data);
+                            ScrapperService.createProgramme(data, keyWords, function() {
+                                callback();
+                            });
+                        }
+                    });
+                }, function() {
+                    callbackGeneral(dayData);
+                });
+
+            });
+        });
+    },
+
+    createProgramme: function(data, keyWords, callback) {
+        Programme.create(data, function(err, programme) {
+            var clean = ToolsService.clean(programme.description) + " " + ToolsService.clean(programme.titre);
+            var keywords = _.filter(keyWords, function(keyword) {
+                return clean.indexOf(keyword) > -1;
+            });
+            console.log(keywords);
+            if (keywords.length > 0) {
+                _.each(keywords, function(keyword, index) {
+                    KeywordProgramme.create({
+                        keyword: keyword,
+                        programme: programme.id
+                    }, function(err, elem) {
+                        if (index == keywords.length - 1)
+                            callback();
+                    });
+                });
+            } else
+                callback();
+        });
+    }
+}
